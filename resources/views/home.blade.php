5165f4ff975fb0c3ea6f55e0a0e1a6ae60a329af
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2022-05-23 19:47:17 +0200

    Showing the types of currency available to process a payment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Make a Payment</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            @csrf
+                            <div class="row">
+                                <div class="col-auto">
+                                    <label for="">How Much You Want to Pay</label>
+                                    <input type="number" name="value" class="form-control" min="5" step="0.01"
+                                        value="{{ mt_rand(500, 100000) / 100 }}" required>
+                                </div>
+                                <div class="col-auto">
+                                    <label for=""> Currency</label>
+                                    <br>
+                                <select class="custom-select" name="currency" required>
 
-                    {{ __('You are logged in!') }}
+                                    @foreach ($currencies as $currency)
+                                        <option value="{{ $currency->iso }}">{{ strtoupper($currency->iso) }}</option>
+
+                                     @endforeach
+
+                                </select>
+
+                                </div>
+                            </div>
+
+                            <div class="text-center mt-3">
+                                <button type="submit" id="payButton" class="btn btn-primary btn-lg">
+                                    Pay
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
